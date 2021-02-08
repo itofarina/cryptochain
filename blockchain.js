abec4b2cd7b1f590974055d6c9e73050f3c185d9
Alvaro Fariña <farinalvaro@gmail.com>
2021-02-08 17:18:59 -0300

    feat: prevents difficulty jumps

--- blockchain.js
+++ blockchain.js
@@ -38,12 +38,15 @@ class Blockchain {
       const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[index];
 
       const actualLastHash = chain[index - 1].hash
+      const lastDifficulty = chain[index - 1].difficulty
 
       if (lastHash !== actualLastHash) return false
 
       const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty)
 
       if (hash !== validatedHash) return false
+
+      if (Math.abs(lastDifficulty - difficulty) > 1) return false
     }
     return true
   }
