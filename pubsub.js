d35027fa64bd1e31253abdf522cdfbd6984c204d
Alvaro Fariña <farinalvaro@gmail.com>
2021-03-07 23:28:39 -0300

    feat: broadcasts chain

--- pubsub.js
+++ pubsub.js
@@ -16,11 +16,14 @@ const credentials = {
 }
 
 const CHANNELS = {
-  TEST: 'TEST'
+  TEST: 'TEST',
+  BLOCKCHAIN: 'BLOCKCHAIN'
 }
 
 class PubSub {
-  constructor() {
+  constructor({ blockchain }) {
+    this.blockchain = blockchain
+
     this.pubnub = new PubNub(credentials)
     this.pubnub.subscribe({ channels: Object.values(CHANNELS) })
     this.pubnub.addListener(this.listener())
@@ -32,6 +35,12 @@ class PubSub {
         const { channel, message } = messageObject
 
         console.log(`Message received. Channel: ${channel}. Message: ${message}`)
+
+        const parsedMessage = JSON.parse(message)
+
+        if (channel === CHANNELS.BLOCKCHAIN) {
+          this.blockchain.replaceChain(parsedMessage)
+        }
       }
     }
   }
@@ -40,9 +49,12 @@ class PubSub {
     this.pubnub.publish({ channel, message })
   }
 
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain)
+    })
+  }
 }
-
-const testPubSub = new PubSub()
-testPubSub.publish({ channel: CHANNELS.TEST, message: 'hello pubnub' })
 
 module.exports = PubSub
