678e3c5c8c567da4cb74854868a69f994ca7c7f9
Alvaro Fariña <farinalvaro@gmail.com>
2021-04-11 20:51:29 -0300

    feat: signs data and verifies signatures

--- wallet/intex.test.js
+++ wallet/intex.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('./index')
+const { verifySignature } = require('../util')
 
 describe('Wallet', () => {
   let wallet
@@ -7,13 +8,36 @@ describe('Wallet', () => {
     wallet = new Wallet()
   })
 
-   it('has a balance', () => {
-     expect(wallet).toHaveProperty('balance')
-   })
+  it('has a balance', () => {
+    expect(wallet).toHaveProperty('balance')
+  })
 
-   it('has a publicKey', () => {
+  it('has a publicKey', () => {
     expect(wallet).toHaveProperty('publicKey')
-   })
+  })
 
-   
+  describe('signing data', () => {
+    const data = 'foobar'
+
+    it('verifies a signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: wallet.sign(data)
+        })
+      ).toBe(true)
+    })
+
+    it('does not verify an invalid signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: new Wallet().sign(data)
+        })
+      ).toBe(false)
+    })
+
+  })
 })
