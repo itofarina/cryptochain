7a09dc3b298910b8a3091bd6744b9098a513d1c4
Alvaro Fariña <farinalvaro@gmail.com>
2021-05-04 17:52:46 -0300

    fix: prevents self-broadcasts on transactions

--- app/pubsub.js
+++ app/pubsub.js
@@ -22,9 +22,10 @@ const CHANNELS = {
 }
 
 class PubSub {
-  constructor({ blockchain, transactionPool }) {
+  constructor({ blockchain, transactionPool, wallet }) {
     this.blockchain = blockchain
     this.transactionPool = transactionPool
+    this.wallet = wallet
 
     this.pubnub = new PubNub(credentials)
     this.pubnub.subscribe({ channels: Object.values(CHANNELS) })
@@ -45,7 +46,11 @@ class PubSub {
             this.blockchain.replaceChain(parsedMessage)
             break
           case CHANNELS.TRANSACTION:
-            this.transactionPool.setTransaction(parsedMessage)
+            if (!this.transactionPool.existingTransaction({
+              inputAddress: this.wallet.publicKey
+            })) {
+              this.transactionPool.setTransaction(parsedMessage)
+            }
             break
           default:
             break
